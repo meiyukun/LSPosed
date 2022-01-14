2f037e2caaa27411c58a79ec75ee6010ef5974b5
青 <meiyukun@vip.qq.com>
2022-01-15 02:06:06 +0800

    添加了dlopen的hook

--- core/src/main/cpp/main/src/hookapi/MyNativeUtils.h
+++ core/src/main/cpp/main/src/hookapi/MyNativeUtils.h
@@ -3,6 +3,7 @@
 #define MYNATIVEUTILS_H
 #include "../../include/base/object.h"
 #include "../../include/native_util.h"
+#include "../symbol_cache.h"
 #include <jni.h>
 
 namespace QyTool{
--- core/src/main/cpp/main/src/hookapi/cocos2d/cocos2dhook.cpp
+++ core/src/main/cpp/main/src/hookapi/cocos2d/cocos2dhook.cpp
@@ -1,5 +1,6 @@
 #include "../MyNativeUtils.h"
 #include "cocos2dhook.h"
+#include <string.h>
 //extern "C"
 //JNIEXPORT void JNICALL
 //Java_com_qingyan_natives_LspNative_setAccessible(JNIEnv* env,jclass clazz, jlong address,jlong length) {
@@ -7,13 +8,37 @@
 //           length);
 //}
 using namespace lspd;
+namespace cocos2djs{
+    const char* LIB_COCOS2DJS_NAME="libcocos2djs.so";
+    const char* SYM_64="_ZN2se12ScriptEngine10evalStringEPKclPNS_5ValueES2_";
+    const char* SYM_32="_ZN2se12ScriptEngine10evalStringEPKciPNS_5ValueES2_";
+    void * (*ori_dlopen)(const char* path,int flag,void *a,void *b)= nullptr;
 
-void hookCocos2dEval(){
+    void* fake_dlopen(const char* path,int flag,void *a,void *b){
+        const char* sym;
+#ifdef __aarch64__
+        LOGE("我是64位");
+        sym=SYM_64;
+#else
+        sym=SYM_32;
+#endif
+        auto x=strstr(path,LIB_COCOS2DJS_NAME);
+        if (x!= nullptr){
 
+        }
+        return ori_dlopen(path,flag,a,b);
+    }
+    void hookCocos2dEval(){
+        HookFunction(symbol_cache->do_dlopen,(void *)fake_dlopen,reinterpret_cast<void **>(&ori_dlopen));
+    }
 }
 
+void doTest() {
+}
+
+
 static JNINativeMethod gMethods[] = {
-         {"hookCocos2dEval", "()V", (void *)hookCocos2dEval}
+         {"hookCocos2dEval", "()V", (void *)cocos2djs::hookCocos2dEval}
 //         {"onEvalString","(Ljava/lang/String;)Ljava/lang/String;",(void *)onEvalString}
 
 };
